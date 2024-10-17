1ecaaf918ab8c1d470e2f65246859337290f0274
poly000 <1348292515a@gmail.com>
2024-10-17 18:56:38 +0800

    feat: record register time

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from typing import Optional
 
 import redis.asyncio as redis
@@ -21,6 +22,7 @@ redis_host = os.getenv("REDIS_HOST")
 redis_port = int(os.getenv("REDIS_PORT"))
 redis_db = int(os.getenv("REDIS_DB"))
 redis_lookup_db = int(os.getenv("REDIS_LOOKUP_DB"))
+redis_time_db = int(os.getenv("REDIS_TIME_DB"))
 
 # 配置 Redis 数据库
 r = redis.StrictRedis(
@@ -29,11 +31,21 @@ r = redis.StrictRedis(
 r_lookup = redis.StrictRedis(
     host=redis_host, port=redis_port, db=redis_lookup_db, password=redis_password
 )
+r_time = redis.StrictRedis(
+    host=redis_host, port=redis_port, db=redis_time_db, password=redis_password
+)
 
 
 # 请求体模型
 class RegistrationRequest(BaseModel):
+    """
+    :serial_number:     激活码
+    :registration_code: 机器码
+    """
+
+    # 激活码
     serial_number: str
+    # 机器码
     registration_code: str
 
 
@@ -50,6 +62,7 @@ class ValidateResponse(BaseModel):
     used: bool
     error: Optional[str]
     regkey: Optional[str]
+    regtime: Optional[str]
 
 
 class ReverseRequest(BaseModel):
@@ -58,6 +71,7 @@ class ReverseRequest(BaseModel):
 
 class ReverseResponse(BaseModel):
     serial_number: Optional[str]
+    register_time: Optional[str]
     error: Optional[str]
 
 
@@ -66,10 +80,11 @@ async def reverse(data: ReverseRequest):
     regcode = data.registeration_code
     try:
         sn = await r_lookup.get(regcode)
+        reg_time = await r_time.get(sn)
     except redis.ConnectionError:
-        return {"error": REDIS_ERROR, "serial_number": None}
+        return {"error": REDIS_ERROR, "serial_number": None, "register_time": None}
 
-    return {"error": None, "serial_number": sn}
+    return {"error": None, "serial_number": sn, "register_time": reg_time}
 
 
 @app.post("/validate", response_model=ValidateResponse)
@@ -81,14 +96,23 @@ async def validate(data: ValidateRequest):
     except redis.ConnectionError:
         return {"error": REDIS_ERROR, "used": False}
 
-    re = {"error": None, "used": bool(existing_code), "regkey": None}
+    result = {
+        "error": None,
+        "used": bool(existing_code),
+        "regkey": None,
+        "regtime": None,
+    }
 
     if existing_code is None:  # not existing
-        re["error"] = KEY_NOT_EXISTING
+        result["error"] = KEY_NOT_EXISTING
     else:
-        re["regkey"] = existing_code.decode("utf-8")
+        result["regkey"] = existing_code.decode("utf-8")
 
-    return re
+    regtime = await r_time.get(sn)
+    if regtime is not None:
+        result["regtime"] = regtime.decode("utf-8")
+
+    return result
 
 
 @app.post("/register", response_model=RegistrationResponse)
@@ -118,6 +142,9 @@ async def register(data: RegistrationRequest):
         try:
             await r.set(serial_number, registration_code)
             print(registration_code, serial_number)
+
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            await r_time.set(serial_number, now)
             await r_lookup.set(registration_code, serial_number)
         except redis.ConnectionError:
             return {"error": REDIS_ERROR, "verified": False}
