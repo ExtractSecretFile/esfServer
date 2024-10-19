0abce010d412bb20a29d3034e8847bd95cce56e0
poly000 <1348292515a@gmail.com>
2024-10-19 21:25:21 +0800

    fix: generate serial numbers

--- gen_serial.py
+++ gen_serial.py
@@ -25,14 +25,12 @@ def generate_serial_number():
     return str(uuid.uuid5(uuid_, "authkey"))[:8]
 
 
-def add_new_serial_numbers(count_):
-    i = 0
-    while i < count_:
+def add_new_serial_numbers(count):
+    for _ in range(count):
         serial_number = generate_serial_number()
         try:
             sn = r.get(serial_number)
             if sn is not None:
-                count_ += 1
                 continue
             # 空 key 表示未注册
             r.set(serial_number, "")
@@ -41,8 +39,6 @@ def add_new_serial_numbers(count_):
             print("连接失败！")
             break
         print(f"{serial_number}")
-
-        i += 1
 
 
 def main():
